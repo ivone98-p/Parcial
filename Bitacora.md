bdcefafc979608aa537e0a1d6f1cb4fa6dfdb5e0
Ivone Romero <ivoneromero9@gmail.com>
2025-03-01 08:30:13 -0500

    v0.7

--- Bitacora.md
+++ Bitacora.md
@@ -10,3 +10,20 @@ configurar user.name
 Crear una pagina que muestre un mapa centrado en mi barrio
 ## solucion
 Utilizar una biblioteca de visor geográfico: leaflet, openlayers, mapopen
+
+# problema 5
+crear un boton y asignarle un manejador de eventos
+
+# problema 6
+¿Donde esta el geojson de los arboles de bogota
+# solucion 
+estan en https://datosabiertos.bogota.gov.co/dataset/censo-arbolado-urbano/resource/2c0d5b2c-dc4c-448b-9573-25864525845c
+
+# pronlema 7
+crear el boudingbos de mi barrio
+
+# problema 8
+descargar los datos filtrados desdela api, sericio rest
+
+# problema
+buscar la interseccion entre los arboles y bbox
--- programa.js
+++ programa.js
@@ -3,3 +3,57 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+// async function cargarGeoJSON() {
+//     try {
+//         let response = await fetch('barrios-bogota'); // Verifica que esta URL sea válida
+//         if (!response.ok) {
+//             throw new Error(`Error HTTP: ${response.status}`);
+//         }
+//         let data = await response.json(); // Esperar el JSON correctamente
+//         L.geoJSON(data).addTo(map); // Agregar los datos al mapa
+//     } catch (error) {
+//         console.error("Error al cargar el GeoJSON:", error);
+//     }
+// }
+
+// cargarGeoJSON();
+
+
+async function loadPolygon(){
+    let myData = await fetch('barrios-bogota.geojson'); //Aquí se especifica .geojson, lo que indica claramente el tipo de archivo que se está solicitando. 
+    let myPolygon = await myData.json();
+
+    L.geoJSON(myPolygon,
+        {
+            style:{
+                color:'blue'
+        }
+        } 
+    ).addTo(map);
+    }
+
+loadPolygon();
+//CARGAR LOS ARBOLES
+async function loadPolygon2() {
+    let myData2 = await fetch('arboles_laEstrada.geojson');  
+    let myPoint = await myData2.json();
+
+    L.geoJSON(myPoint, {
+        pointToLayer: function(feature, latlng) {
+            return L.circleMarker(latlng, {
+                radius: 5,  // Tamaño del punto
+                color: 'green',  // Borde del punto
+                fillColor: 'green',  // Color del relleno
+                fillOpacity: 0.8  // Opacidad del punto
+            });
+        }
+    }).addTo(map);
+}
+
+loadPolygon2();
+
+
+
+let btnTrees = document.getElementById("btnTrees");
+btnTrees.addEventListener('click', ()=> alert('Hola'));
